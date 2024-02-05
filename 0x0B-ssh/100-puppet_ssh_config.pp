49e72e0a59bfaf81d6acd4bd08748c9d8db835e9
DeeGemini <zodiactheegemini@gmail.com>
2024-02-04 16:03:48 -0800

    Updated

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
-# UsingPuppet to connect without the password
-
-file { '/home/ubuntu/.ssh/config':
-  ensure  => file,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0600',
-  content => "Host *\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no",
-}
+ssh-copy-id ubuntu@54.242.199.148
 
