d12191c9ff7404bcec865aa96aebd0e37442b86d
DeeGemini <zodiactheegemini@gmail.com>
2024-03-05 14:37:48 -0800

    Provides a function to query the Reddit API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module provides a function to query the Reddit API and return the number of subscribers for a given subreddit.
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers, or 0 if the subreddit is invalid.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'MyBot/0.1'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return (data['data']['subscribers'])
+    else:
+        return (0)
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+
